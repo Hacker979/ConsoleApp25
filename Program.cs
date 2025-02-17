b8c2f72e7469acd8bcbdbde14df90a581567a044
jotaro4u <kothaker135@gmail.com>
2025-02-17 22:05:27 +0300

    sf

--- Program.cs
+++ Program.cs
@@ -10,33 +10,10 @@ namespace ConsoleApp25
     {
         static void Main(string[] args)
         {
-            //Console.Write("b = ");
-            //int b = int.Parse(Console.ReadLine());
-
-            //int[] mas = new int[b];
-            //for (int i = 0; i < b; i++)
-            //{
-            //    Console.Write($"mas[{i}]=");
-            //    mas[i] = int.Parse(Console.ReadLine());
-            //}
-            //for (int i=0; i < mas.Length; i++)
-            //{
-            //    Console.WriteLine($"mas[{i}]= {mas[i]}");
-            //}
-
-
-
-            int[] mas = new int[5];
-            Random r = new Random();
-            for (int i = 0; i < 5; i++)
-            {
-                mas[i] = r.Next(-10, 10);
-            }
-            foreach (int el in mas)
-            {
-                Console.WriteLine($"{el}\t");
-            }
-            Console.Read();
+            // 1) Инициализация массива без указания значений
+            int[] mass = new int[5];
+            foreach (int i in mass)
+                Console.Write(i + " ");
         }
     }
 }
